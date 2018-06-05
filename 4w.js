2bbc96ffd978c22a4b31fe75ce56f5e8b4473cac
William Wiechorek <william@sprinta.com.br>
2018-06-05 15:24:23 -0300

    Remover e adicionar headings

--- 4w.js
+++ 4w.js
@@ -1,6 +1,117 @@
 function w4( selector ) {
+/*third-party*/
 
-   listeners = {}
+      function CleanPastedHTML(input) {
+            // 1. remove line breaks / Mso classes
+            var stringStripper = /(\n|\r| class=(")?Mso[a-zA-Z]+(")?)/g;
+            var output = input.replace(stringStripper, ' ');
+            // 2. strip Word generated HTML comments
+            var commentSripper = new RegExp('<!--(.*?)-->','g');
+            var output = output.replace(commentSripper, '');
+            var tagStripper = new RegExp('<(/)*(meta|link|span|\\?xml:|st1:|o:|font)(.*?)>','gi');
+            // 3. remove tags leave content if any
+            output = output.replace(tagStripper, '');
+            // 4. Remove everything in between and including tags '<style(.)style(.)>'
+            var badTags = ['style', 'script','applet','embed','noframes','noscript'];
+
+            for (var i=0; i< badTags.length; i++) {
+                  tagStripper = new RegExp('<'+badTags[i]+'.*?'+badTags[i]+'(.*?)>', 'gi');
+                  output = output.replace(tagStripper, '');
+            }
+            // 5. remove attributes ' style="..."'
+            var badAttributes = ['style', 'start'];
+            for (var i=0; i< badAttributes.length; i++) {
+                  var attributeStripper = new RegExp(' ' + badAttributes[i] + '="(.*?)"','gi');
+                  output = output.replace(attributeStripper, '');
+            }
+
+            return output;
+      }
+
+      function getRangeSelectedNodes(range, includePartiallySelectedContainers) {
+            var node = range.startContainer;
+            var endNode = range.endContainer;
+            var rangeNodes = [];
+
+            // Special case for a range that is contained within a single node
+            if (node == endNode) {
+                  rangeNodes = [node];
+            } else {
+                  // Iterate nodes until we hit the end container
+                  while (node && node != endNode) {
+                        rangeNodes.push( node = nextNode(node) );
+                  }
+
+                  // Add partially selected nodes at the start of the range
+                  node = range.startContainer;
+                  while (node && node != range.commonAncestorContainer) {
+                        rangeNodes.unshift(node);
+                        node = node.parentNode;
+                  }
+            }
+
+            // Add ancestors of the range container, if required
+            if (includePartiallySelectedContainers) {
+                  node = range.commonAncestorContainer;
+                  while (node) {
+                        rangeNodes.push(node);
+                        node = node.parentNode;
+                  }
+            }
+
+            return rangeNodes;
+      }
+
+      function getSelectedNodes() {
+            var nodes = [];
+            if (window.getSelection) {
+                  var sel = window.getSelection();
+                  for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                        nodes.push.apply(nodes, getRangeSelectedNodes(sel.getRangeAt(i), true));
+                  }
+            }
+            return nodes;
+      }
+
+      function replaceWithOwnChildren(el) {
+            var parent = el.parentNode;
+            while (el.hasChildNodes()) {
+                  parent.insertBefore(el.firstChild, el);
+            }
+            parent.removeChild(el);
+      }
+
+      function removeSelectedElements(tagNames) {
+            var tagNamesArray = tagNames.toLowerCase().split(",");
+            getSelectedNodes().forEach(function(node) {
+                  if (node.nodeType == 1 &&
+                  tagNamesArray.indexOf(node.tagName.toLowerCase()) > -1) {
+                        // Remove the node and replace it with its children
+                        replaceWithOwnChildren(node);
+                  }
+            });
+      }
+      
+/*third-party*/
+
+   var listeners = {}
+   var properties = {
+      'bold' : false,
+      'italic' : false,
+      'underline' : false,
+      'justifyleft' : false,
+      'justifycenter' : false,
+      'justifyright' : false,
+      'insertunorderedlist' : false,
+      'insertorderedlist' : false,
+      'link' : false,
+      'h1' : false,
+      'h2' : false,
+      'h3' : false,
+      'h4' : false,
+      'h5' : false,
+      'h6' : false
+   }
 
    function setFocus() {
       editor.focus()      
@@ -8,40 +119,31 @@ function w4( selector ) {
 
    function ExecCommand(command, data) {
       if(command === 'h1' || command === 'h2' || command === 'h3' || command === 'h4' || command === 'h5' || command === 'h6') {
-            document.execCommand('formatBlock', false, '<'+command+'>')
+            if(properties[command]) {
+                  removeSelectedElements(command);      
+            } else {     
+                  document.execCommand('formatBlock', false, '<'+command+'>')
+            }
             setFocus()            
       }
 
       document.execCommand(command, false, data)
       setFocus()
    }
-   
-   function CleanPastedHTML(input) {
-      // 1. remove line breaks / Mso classes
-      var stringStripper = /(\n|\r| class=(")?Mso[a-zA-Z]+(")?)/g;
-      var output = input.replace(stringStripper, ' ');
-      // 2. strip Word generated HTML comments
-      var commentSripper = new RegExp('<!--(.*?)-->','g');
-      var output = output.replace(commentSripper, '');
-      var tagStripper = new RegExp('<(/)*(meta|link|span|\\?xml:|st1:|o:|font)(.*?)>','gi');
-      // 3. remove tags leave content if any
-      output = output.replace(tagStripper, '');
-      // 4. Remove everything in between and including tags '<style(.)style(.)>'
-      var badTags = ['style', 'script','applet','embed','noframes','noscript'];
-   
-      for (var i=0; i< badTags.length; i++) {
-         tagStripper = new RegExp('<'+badTags[i]+'.*?'+badTags[i]+'(.*?)>', 'gi');
-         output = output.replace(tagStripper, '');
+
+   function nextNode(node) {
+      if (node.hasChildNodes()) {
+          return node.firstChild;
+      } else {
+          while (node && !node.nextSibling) {
+              node = node.parentNode;
+          }
+          if (!node) {
+              return null;
+          }
+          return node.nextSibling;
       }
-      // 5. remove attributes ' style="..."'
-      var badAttributes = ['style', 'start'];
-      for (var i=0; i< badAttributes.length; i++) {
-         var attributeStripper = new RegExp(' ' + badAttributes[i] + '="(.*?)"','gi');
-         output = output.replace(attributeStripper, '');
-      }
-      
-      return output;
-   }
+  }
    
    function handleKeyDown(e) {
       var ctrl = e.ctrlKey || e.metaKey
@@ -114,24 +216,6 @@ function w4( selector ) {
       }
 
       return false
-   }
-
-   var properties = {
-      'bold' : false,
-      'italic' : false,
-      'underline' : false,
-      'justifyleft' : false,
-      'justifycenter' : false,
-      'justifyright' : false,
-      'insertunorderedlist' : false,
-      'insertorderedlist' : false,
-      'link' : false,
-      'h1' : false,
-      'h2' : false,
-      'h3' : false,
-      'h4' : false,
-      'h5' : false,
-      'h6' : false
    }
    
    function readProperties() {
@@ -207,8 +291,8 @@ function w4( selector ) {
       setTimeout(function() {
          if(lastContent !== editor.innerHTML) {
             
-            if(this.listeners.change)
-               this.listeners.change(editor.innerHTML)
+            if(listeners.change)
+               listeners.change(editor.innerHTML)
 
             lastContent = editor.innerHTML
          }
@@ -248,8 +332,8 @@ function w4( selector ) {
 
    var imgs = editor.querySelectorAll('img')
    for (let index = 0; index < imgs.length; index++) {
-         const element = imgs[index];
-         element.addEventListener('DOMNodeRemovedFromDocument', onImageRemove, false);
+      const element = imgs[index];
+      element.addEventListener('DOMNodeRemovedFromDocument', onImageRemove, false);
    }
 }
 
